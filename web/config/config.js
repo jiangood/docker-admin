1a054e4e19d7517a8d964c3ef0b7a6da79f154e9
jiangood <410518072@qq.com>
2026-01-04 15:13:48 +0800

    upgrade: 升级框架

--- web/config/config.js
+++ web/config/config.js
@@ -2,11 +2,6 @@ import {defineConfig} from 'umi';
 import config from "@jiangood/springboot-admin-starter/config/dist/config";
 
 config. proxy= {
-
-    '/admin': {
-        target: 'http://127.0.0.1:8002',
-            changeOrigin: true,
-    },
     '/admin/ws': {
         target: 'http://127.0.0.1:8002',
         changeOrigin: true,
