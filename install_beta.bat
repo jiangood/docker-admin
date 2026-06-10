a76a8d2c1fea25a174011d65e69540d3c8302182
jiangood <410518072@qq.com>
2026-06-10 09:10:24 +0800

    chore: remove install_beta.bat

--- install_beta.bat
+++
@@ -1,9 +0,0 @@
-chcp 65001 > nul
-@echo off
-
-
-echo 中文显示测试
-cd web
-call pnpm install @jiangood/open-admin@beta --registry https://packages.aliyun.com/62d39be70065edd3d51c1984/npm/npm-registry/
-
-
